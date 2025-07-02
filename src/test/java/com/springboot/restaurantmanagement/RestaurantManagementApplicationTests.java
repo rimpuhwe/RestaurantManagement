44fd97719f576f1d98085171cd725a40e78e40a4
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-07-02 13:17:15 +0200

    Intial commit

---
+++ src/test/java/com/springboot/restaurantmanagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restaurantmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
