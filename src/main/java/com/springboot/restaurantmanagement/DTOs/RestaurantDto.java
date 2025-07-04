c5929edc53dcea54e28aaad4e70d05922c693ba5
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-07-04 11:26:18 +0200

    Refactor : add the name variable

--- src/main/java/com/springboot/restaurantmanagement/DTOs/RestaurantDto.java
+++ src/main/java/com/springboot/restaurantmanagement/DTOs/RestaurantDto.java
@@ -5,6 +5,7 @@ import com.springboot.restaurantmanagement.Enums.RestaurantStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 import lombok.Value;
 
 import java.io.Serializable;
@@ -12,15 +13,21 @@ import java.io.Serializable;
 /**
  * DTO for {@link Restaurant}
  */
-@Value
+@Data
 public class RestaurantDto{
     @NotNull
     @NotBlank(message = "provide the restaurant's address")
     String address;
+
     @NotNull
-    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$\n", message = "phone number must be valid ")
+    @NotBlank(message = "Restaurant's name is required")
+    String name;
+
+    @NotNull
+    @Pattern(regexp = "^\\+[1-9][0-9]{7,14}$", message = "Phone number must be in international format")
     @NotBlank(message = "this field is required")
     String phone;
+
     @NotNull(message = "status field required ")
     RestaurantStatus status;
 }
