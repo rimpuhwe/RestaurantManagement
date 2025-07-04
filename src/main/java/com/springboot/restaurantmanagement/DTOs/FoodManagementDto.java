6b467739830955448fc87bbef36e1503b64aaf8d
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-07-04 13:53:22 +0200

    Feat: add dtos that will help in food and drinks operations

--- src/main/java/com/springboot/restaurantmanagement/DTOs/FoodManagementDto.java
+++ src/main/java/com/springboot/restaurantmanagement/DTOs/FoodManagementDto.java
@@ -1,7 +1,18 @@
 package com.springboot.restaurantmanagement.DTOs;
 
+import com.springboot.restaurantmanagement.Enums.FoodCategory;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Value;
+
+import java.io.Serializable;
+
 /**
  * DTO for {@link com.springboot.restaurantmanagement.Entities.FoodManagement}
  */
-public class FoodManagementDto {
-  }
+@Data
+public class FoodManagementDto extends MenuDto {
+
+    @NotNull
+    FoodCategory category;
+}
