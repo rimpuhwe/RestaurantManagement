d8abafcb7a32318e8433453454e6a9456fb97917
CYNTHIA-IGIHOZO <ciiginta7@gmail.com>
2025-07-07 14:48:58 +0200

    feat: implement OrderItemService for saving and retrieving order items

--- src/main/java/com/springboot/restaurantmanagement/Services/OrderItemService.java
+++ src/main/java/com/springboot/restaurantmanagement/Services/OrderItemService.java
@@ -1,4 +1,28 @@
 package com.springboot.restaurantmanagement.Services;
 
+import com.springboot.restaurantmanagement.Entities.OrderItem;
+import com.springboot.restaurantmanagement.Repositories.OrderItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class OrderItemService {
+
+    private final OrderItemRepository orderItemRepository;
+
+    public List<OrderItem> getItemsByOrder(Long orderId) {
+        return orderItemRepository.findByOrderId(orderId);
+    }
+
+    public OrderItem saveItem(OrderItem item) {
+        return orderItemRepository.save(item);
+    }
+
+    public void deleteItem(Long id) {
+        orderItemRepository.deleteById(id);
+    }
 }
+
