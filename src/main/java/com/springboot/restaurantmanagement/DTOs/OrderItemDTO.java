a7862937e9b582c619ff5cba632f4911ec15cb7d
CYNTHIA-IGIHOZO <ciiginta7@gmail.com>
2025-07-07 14:48:49 +0200

    feat: define OrderItemDTO to encapsulate item-level details in orders

--- src/main/java/com/springboot/restaurantmanagement/DTOs/OrderItemDTO.java
+++ src/main/java/com/springboot/restaurantmanagement/DTOs/OrderItemDTO.java
@@ -1,4 +1,15 @@
 package com.springboot.restaurantmanagement.DTOs;
 
-public class OrderItemDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDTO {
+    private Long productId;
+    private int quantity;
+    private double price;
 }
+
