261a983749b990beadd4c424ad7da5e2d25bcb43
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-07-08 12:16:47 +0200

    Feat: add the dto only used for updating

--- src/main/java/com/springboot/restaurantmanagement/DTOs/DrinksUpdateDto.java
+++ src/main/java/com/springboot/restaurantmanagement/DTOs/DrinksUpdateDto.java
@@ -1,7 +1,20 @@
 package com.springboot.restaurantmanagement.DTOs;
 
+import com.springboot.restaurantmanagement.Enums.MenuStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+import java.io.Serializable;
+
 /**
  * DTO for {@link com.springboot.restaurantmanagement.Entities.DrinksManagement}
  */
+@Value
 public class DrinksUpdateDto {
-  }
+    @Min(message = "this field must not be null", value = 0)
+    double price;
+    @NotNull
+    MenuStatus status;
+    int weight;
+}
