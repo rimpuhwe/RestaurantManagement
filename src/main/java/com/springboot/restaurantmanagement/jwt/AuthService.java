b1f2d53dfb356cb0487f2eca9579d58a22848b9d
Hope Gaju <hopegajupr@gmail.com>
2025-07-09 17:22:02 +0200

    feat: add AuthService for JWT-based login and registration handling

--- src/main/java/com/springboot/restaurantmanagement/jwt/AuthService.java
+++ src/main/java/com/springboot/restaurantmanagement/jwt/AuthService.java
@@ -1,4 +1,26 @@
 package com.springboot.restaurantmanagement.jwt;
 
+import com.springboot.restaurantmanagement.DTOs.SignupDto;
+import com.springboot.restaurantmanagement.Entities.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.springboot.restaurantmanagement.Repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+
+    public User registerUser(SignupDto dto) {
+        User user = new User();
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setFullName(dto.getFullName());
+        return userRepository.save(user);
+    }
 }
+
