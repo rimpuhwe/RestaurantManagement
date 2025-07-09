fd895af6b5d7ab3e2cafc4328c5b0e4f99cef08e
Hope Gaju <hopegajupr@gmail.com>
2025-07-09 17:21:58 +0200

    feat: add SignupDto for user registration data structure

--- src/main/java/com/springboot/restaurantmanagement/DTOs/SignupDto.java
+++ src/main/java/com/springboot/restaurantmanagement/DTOs/SignupDto.java
@@ -1,4 +1,14 @@
 package com.springboot.restaurantmanagement.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignupDto {
+    private String fullName;
+    private String email;
+    private String password;
 }
