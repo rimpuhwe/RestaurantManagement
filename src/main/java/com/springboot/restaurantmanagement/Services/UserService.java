aa47aeecdc5f5a8310ef05a53c13e4060f5dc28d
Hope Gaju <hopegajupr@gmail.com>
2025-07-09 17:22:01 +0200

    feat: implement UserService for user logic and authentication flow

--- src/main/java/com/springboot/restaurantmanagement/Services/UserService.java
+++ src/main/java/com/springboot/restaurantmanagement/Services/UserService.java
@@ -1,4 +1,17 @@
-package user;
+package com.springboot.restaurantmanagement.Services;
+import com.springboot.restaurantmanagement.Entities.User;
+import com.springboot.restaurantmanagement.Repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
 }
