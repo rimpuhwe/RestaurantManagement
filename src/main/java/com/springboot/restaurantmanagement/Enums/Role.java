28b8eb1fdbc5a0fce9c1a8205a552d4987643a0c
Hope Gaju <hopegajupr@gmail.com>
2025-07-10 12:13:04 +0200

    feat: add Role enum to define user roles

--- src/main/java/com/springboot/restaurantmanagement/Enums/Role.java
+++ src/main/java/com/springboot/restaurantmanagement/Enums/Role.java
@@ -1,4 +1,8 @@
 package com.springboot.restaurantmanagement.Enums;
 
 public enum Role {
+    SUPER_ADMIN,    // Can manage all branches
+    BRANCH_MANAGER, // Can manage single branch
+    WAITER          // Can manage assigned tables/orders
+
 }
